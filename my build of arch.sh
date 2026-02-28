ffabc7ad49fe6aab0039efdb3cbd838b1e93757b
port dir <portdirfile@outlook.com>
2026-02-28 06:52:29 -0800

    Update my build of arch.sh

--- my build of arch.sh
+++ my build of arch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # -------------------------------
 # casts profle updater for arch 
-# v2.6
+# v2.7
 # -------------------------------
 cd ~/Desktop/
 echo "
@@ -14,6 +14,7 @@ thing that happen.
 # ~/.bashrc - thay are CMDs that i use like 'yak' this types out stuff after the cmd like copy and paste
 # adds mounts at /mnt/mount{1,2,3,4,5,6,7}
 ---- installs ----
+# steam <-[working on]
 # yay
 # git 
 # firewalld
@@ -25,9 +26,11 @@ thing that happen.
 # most
 # figlet
 # yt-dlp
-# bandcamp-dl-git soundcloud-dl-git - funny
+# bandcamp-dl-git + soundcloud-dl-git <-[funny]
 ------------------
 "
+echo "add the !funny!? [Y/N]" 
+read addfunny 
 echo "! hit any key to keep going to install. !"
 read moveon
 # network stuff
@@ -62,10 +65,7 @@ if [[ $yesno =~ ^(Y|y|yes)$ ]]; then
 # ====================================
 # update bashrc file 
   echo "
-        # --------------------------------------
-        # the things
-        # --------------------------------------
-        # olther
+        # things     --------------------------------------
           alias hello='figlet UWU HELLOWO'
           alias grep='grep --color=auto'
           alias grubup='sudo grub-mkconfig -o /boot/grub/grub.cfg'
@@ -73,21 +73,25 @@ if [[ $yesno =~ ^(Y|y|yes)$ ]]; then
           alias aliasup='source ~/.bashrc'
           alias pacman='sudo pacman'
           alias yak='xdotool sleep 2 type --delay 10'
-        # olther end
-        # funny steal
-          alias Ssteal='scdl --original-art --yt-dlp-args '--embed-thumbnail' -l'
-          alias Bsteal='bandcamp-dl -r -e --base-dir ./'
-          alias Ysteal='yt-dlp -x --embed-thumbnail --embed-metadata --cookies-from-browser firefox --audio-format mp3'
-        # funny steal end
-        # --------------------------------------
+        # olther end --------------------------------------
    " >> ~/.bashrc
  # build 
    source ~/.bashrc
    
  # ====================================
- # funny
-   sudo pacman -S yt-dlp
-   yay -S bandcamp-dl-git soundcloud-dl-git
+ # funny 
+  if [[ $addfunny =~ ^(Y|y|yes)$ ]]; then
+      echo "# funny     --------------------------------------
+              alias Ssteal='scdl --original-art --yt-dlp-args '--embed-thumbnail' -l'
+              alias Bsteal='bandcamp-dl -r -e --base-dir ./'
+              alias Ysteal='yt-dlp -x --embed-thumbnail --embed-metadata --cookies-from-browser firefox --audio-format mp3'
+            # funny end --------------------------------------
+           " > ~/.bashrc
+      sudo pacman -S yt-dlp
+      yay -S bandcamp-dl-git soundcloud-dl-git
+  else
+    echo "no funny"
+  fi
  # funny end
  # ====================================    
 else
